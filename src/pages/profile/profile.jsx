01852b0f2c092bae2332948e0d08e5596e369506
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-10-24 17:05:06 +0700

    on progres profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -8,6 +8,9 @@ import haris from "../../assets/images/harispp.png";
 import { H4tag, Span14 } from "../../components/Atom/text/text";
 import { ButtonPrimary } from "../../components/Atom/button/button";
 import About from "./about";
+import ProfileFeed from "./profileFeed";
+import { SidebarGallery } from "../../components/Molekul/right-sidebar/right-sidebar";
+import { ContactOnly } from "../../components/Molekul/contact/contact";
 
 const Profile = () => {
   return (
@@ -45,14 +48,53 @@ const Profile = () => {
           <ButtonPrimary new_className={styles.hiddenStyle} text={"Groups"} />
         </div>
       </div>
+      {/* POST */}
       <div className={styles.main_section_profile}>
-        <div className={styles.profileLeft}></div>
-        <div className={styles.profileMid}></div>
-        <div className={styles.profileRight}></div>
-        <About/>
+        <div className={styles.profileLeft}>
+          <About />
+        </div>
+        <div className={styles.profileMid}>
+          <ProfileFeed />
+        </div>
+        <div className={styles.profileRight}>
+          {" "}
+          <SidebarGallery />
+          <br />
+          <ContactOnly />
+        </div>
+      </div>
+      {/* ABOUT */}
+      <div className={styles.main_section_profile}>
+        <div className={styles.aboutLeft}>
+          <About />
+        </div>
+        <div className={styles.aboutRight}>
+          {" "}
+          <ContactOnly />
+        </div>
+      </div>
+    {/* photo */}
+      <div className={styles.main_section_profile}>
+        <div className={styles.aboutLeft}>
+          <SidebarGallery />
+        </div>
+        <div className={styles.aboutRight}>
+          {" "}
+          <ContactOnly />
+        </div>
       </div>
     </div>
   );
 };
 
+const Post = () => {
+  return (
+    <div>
+      <About />
+      <ProfileFeed />
+      <SidebarGallery />
+      <ContactOnly />
+    </div>
+  );
+};
 export default Profile;
---
+++ src/pages/profile/profileFeed.jsx
@@ -0,0 +1,11 @@
+import Feed from "../../components/Organism/feed/feed"
+
+const ProfileFeed = () => {
+    return(
+        <div>
+            <Feed/>
+        </div>
+    )
+}
+
+export default ProfileFeed
