5c520f74e4122d0012877e40a9ca3668cdab8bc8
Haris-mlna <harismlnaslm@gmail.com>
2023-10-25 14:04:43 +0700

    update

--- src/components/Organism/navbar-mobile/navbar-mobile.js
+++ src/components/Organism/navbar-mobile/navbar-mobile.js
@@ -1,11 +1,36 @@
-import styles from './navbar-mobile.module.css'
+import { IconsFA } from "../../Atom/icons/icons";
+import { AvatarNavbar } from "../../Atom/image-placeholder/image-placeholder";
+import styles from "./navbar-mobile.module.css";
 
 const NavbarMobile = () => {
   return (
     <div className={styles.navbar}>
-    
+      <ul>
+        <li>
+          <button className={`${styles.notification}`}>
+            <IconsFA title={"mail"} size={"small"} />
+          </button>
+        </li>
+        <li>
+          <button className={`${styles.notification}`}>
+            <IconsFA title={"notifications"} size={"small"} />
+          </button>
+        </li>
+        <li>
+          <AvatarNavbar new_className={styles.avatar} />
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+const DropdownAvatar = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
 export default NavbarMobile;
