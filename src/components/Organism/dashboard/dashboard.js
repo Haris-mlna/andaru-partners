86b6d020d84b4db70ccaaa83936d0b16939ad059
Haris-mlna <harismlnaslm@gmail.com>
2023-10-26 11:34:58 +0700

    update

--- src/components/Organism/dashboard/dashboard.js
+++ src/components/Organism/dashboard/dashboard.js
@@ -1,10 +1,25 @@
-import styles from './dashboard.module.css'
+import styles from "./dashboard.module.css";
+import {  useState } from "react";
 
 const Dashboard = () => {
+
+  const [myOrder , setMyOrder] = useState(true)
+  const [partnerOrder , setPartnerOrder] = useState(false)
+
+  const toggleOrder = (active , nonactive) => {
+    active(true);
+    nonactive(false)
+  }
+
   return (
     <div className={styles.dashboard_container}>
       <div className={styles.head}>
         <h2>Dashboard</h2>
+
+        <div>
+          <button onClick={() => toggleOrder(setMyOrder, setPartnerOrder)} className={`${styles.button_order} ${myOrder ? styles.active : styles.deactive}`}>Pesanan saya</button>
+          <button onClick={() => toggleOrder(setPartnerOrder, setMyOrder)} className={`${styles.button_order} ${partnerOrder ? styles.active : styles.deactive}`}>Pesanan Partner</button>
+        </div>
       </div>
       <div className={styles.dashboard}>
         <div className={styles.left}>
@@ -12,14 +27,13 @@ const Dashboard = () => {
             <div className={styles.myorder}>
               <p>Pesanan saya</p>
             </div>
-            <div className={styles.delivery}>
-            </div>
+            <div className={styles.delivery}></div>
           </div>
         </div>
         <div className={styles.right}></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
