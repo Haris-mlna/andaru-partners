10bdc45f73a40c284e64f3cea513a5e8a103d56f
Haris-mlna <harismlnaslm@gmail.com>
2023-10-28 11:51:29 +0700

    update add delivery

--- src/pages/delivery/delivery.js
+++ src/pages/delivery/delivery.js
@@ -4,7 +4,9 @@ const Delivery = () => {
   return (
     <>
       <div className={styles.pages}>
-        
+        <div className={styles.container}>
+
+        </div>
       </div>
     </>
   );
