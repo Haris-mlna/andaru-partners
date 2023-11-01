7276f18fbaf6160e4599381dfd34d716650eb387
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-11-01 11:27:41 +0700

    upadet invoice

--- src/components/Organism/table-invoice/table.jsx
+++ src/components/Organism/table-invoice/table.jsx
@@ -1,36 +1,47 @@
+import React, { useState } from "react";
+import { dataDelivery } from "../../../data/data";
 import styles from "./table.module.css";
 
-const InvoiceAdd = () => {
+const Card = ({ data }) => {
+  const [showDetail, setShowDetail] = useState(false);
+
+  const toggleDetail = () => {
+    setShowDetail(!showDetail);
+  };
+
   return (
-    <div className={styles.invoiceAdd}>
-      <div className={styles.wrap}>
-        <div className={styles.data}>
-          <div className={styles.fill}>
-            <h6 className={styles.row}>No Invoice</h6>
-            <p className={styles.col}>11111</p>
-          </div>
-
-          <div className={styles.fill}>
-            <h6 className={styles.row}>Pembeli</h6>
-            <p className={styles.col}>111111</p>
-          </div>
-
-          <div className={styles.fill}>
-            <h6 className={styles.row}>Tanggal Invoice</h6>
-            <p className={styles.col}>1111111</p>
-          </div>
-
-          <div className={styles.fill}>
-            <h6 className={styles.row}>Alamat Penagih</h6>
-            <p className={styles.col}>11111111</p>
-          </div>
-
-          <div className={styles.fill}>
-            <h6 className={styles.row}>Status Pesanan</h6>
-            <p className={styles.col}>1111111111</p>
-          </div>
+    <div className={styles.wrap}>
+      <div className={styles.data}>
+        <div className={styles.fill}>
+          <h6 className={styles.row}>No Invoice</h6>
+          <p className={styles.col}>{data.no_Do}</p>
         </div>
-      
+
+        <div className={styles.fill}>
+          <h6 className={styles.row}>Pembeli</h6>
+          <p className={styles.col}>{data.pembeli}</p>
+        </div>
+
+        <div className={styles.fill}>
+          <h6 className={styles.row}>Tanggal Invoice</h6>
+          <p className={styles.col}>{data.metode_pengiriman}</p>
+        </div>
+
+        <div className={styles.fill}>
+          <h6 className={styles.row}>Alamat Penagih</h6>
+          <p className={styles.col}>{data.status_pesanan}</p>
+        </div>
+
+        <div className={styles.fill}>
+          <h6 className={styles.row}>Status Pesanan</h6>
+          <p className={styles.col}>{data.tanggal_pembuatan}</p>
+        </div>
+      </div>
+      <button className="btn-detail" onClick={toggleDetail}>
+        Lihat Detail
+      </button>
+      {showDetail && (
+        <div className="detail-content">
           <div className={styles.history}>
             <div className={styles.historySect}>Detail Pesanan</div>
              <div className={styles.fill}>
@@ -54,7 +65,18 @@ const InvoiceAdd = () => {
                 <p className={styles.col}>1111111111</p>
             </div>
           </div>
-      </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const InvoiceAdd = () => {
+  return (
+    <div className={styles.invoiceAdd}>
+      {dataDelivery.rows.map((data, index) => (
+        <Card data={data} key={index} />
+      ))}
     </div>
   );
 };
