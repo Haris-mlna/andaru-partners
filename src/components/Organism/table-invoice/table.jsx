1baeffd4270e63d00d289d2b627d8033c9e3be78
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-11-01 17:14:14 +0700

    update sore

--- src/components/Organism/table-invoice/table.jsx
+++ src/components/Organism/table-invoice/table.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { dataDelivery } from "../../../data/data";
 import styles from "./table.module.css";
+import { Button } from "@mui/material";
 
 const Card = ({ data }) => {
   const [showDetail, setShowDetail] = useState(false);
@@ -37,34 +38,41 @@ const Card = ({ data }) => {
           <p className={styles.col}>{data.tanggal_pembuatan}</p>
         </div>
       </div>
-      <button className="btn-detail" onClick={toggleDetail}>
+      <div className={styles.btn}>
+      <Button variant="contained" size="small" className={styles.btn_detail} onClick={toggleDetail}>
         Lihat Detail
-      </button>
+      </Button>
+      </div>
+      
       {showDetail && (
-        <div className="detail-content">
-          <div className={styles.history}>
-            <div className={styles.historySect}>Detail Pesanan</div>
+        <div className={styles.detail_content}>
+          <div className={styles.historySect}>Detail Pesanan</div>
+          {data.detail_pesanan_produk.map((detailProduct, index) => (
+            <div className={styles.history} key={index}>
+            
              <div className={styles.fill}>
                 <h6 className={styles.row}>Produk</h6>
-                <p className={styles.col}>1111111111</p>
+                <p className={styles.col}>{detailProduct.product}</p>
             </div>
              <div className={styles.fill}>
                 <h6 className={styles.row}>Kuantitas 1</h6>
-                <p className={styles.col}>1111111111</p>
+                <p className={styles.col}>{detailProduct.kuantitas_1}</p>
             </div>
              <div className={styles.fill}>
                 <h6 className={styles.row}>Kuantitas 2</h6>
-                <p className={styles.col}>1111111111</p>
+                <p className={styles.col}>{detailProduct.kuantitas_2}</p>
             </div>
              <div className={styles.fill}>
                 <h6 className={styles.row}>Kuantitas 3</h6>
-                <p className={styles.col}>1111111111</p>
+                <p className={styles.col}>{detailProduct.kuantitas_3}</p>
             </div>
              <div className={styles.fill}>
                 <h6 className={styles.row}>Total</h6>
                 <p className={styles.col}>1111111111</p>
             </div>
+            <hr />
           </div>
+          ))}
         </div>
       )}
     </div>
