caf40e3a70aceebb15fc477338f3f13aa4b60d26
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-11-04 14:47:41 +0700

    update sabtu

---
+++ src/pages/password-recovery/password-recovery.jsx
@@ -0,0 +1,9 @@
+import styles from './password-recovery.module.css'
+
+const PassRecovery = () => {
+    return(
+        <div className={styles.recovery}></div>
+    )
+}
+
+export default PassRecovery
